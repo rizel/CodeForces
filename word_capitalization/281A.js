4155c27531093cf26da639445ee06ca75e0ebb6e
Rizel Tane <rizelita@gmail.com>
2022-02-24 00:43:12 -0400

    String.slice() to obtain a part of the string.

---
+++ word_capitalization/281A.js
@@ -0,0 +1,29 @@
+// status: ACCEPTED
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map( string => {
+        return string.trim();
+    });
+    main();
+});
+
+function readline() {
+    return inputString[currentLine++];
+}
+
+function main() {
+    const word = readline();
+    console.log(word[0].toUpperCase()+word.slice(1));
+    
+}
